b705814f33f41c3cedb07640275c03592da64e40
simon <simon.h@gamexproject.com>
2026-02-09 00:03:41 +0800

    1

--- pages/main_widget.py
+++ pages/main_widget.py
@@ -1,36 +1,83 @@
+import sys
 from functools import partial
+
 from PySide6.QtCore import Qt, Signal
 from PySide6.QtWidgets import (
-    QWidget, QHBoxLayout,
-    QAbstractItemView, QHeaderView,
-    QTableWidgetItem, QCompleter, QSizePolicy
+    QApplication, QWidget,
+    QTableWidgetItem, QAbstractItemView,
+    QHeaderView, QVBoxLayout, QSizePolicy, QCompleter, QHBoxLayout
 )
+from qfluentwidgets import TableWidget, PrimaryPushButton, SearchLineEdit, PrimaryToolButton, VBoxLayout
+from qfluentwidgets import FluentIcon as FIF
 
-from pages.wallet_widget import WalletWidget
 from service.db_server import DBService
 from utils.tron_sdk_service import TronService
 
-from qfluentwidgets import (
-    PrimaryPushButton, PrimaryToolButton,
-    VBoxLayout, TableWidget, MessageBox,
-    SearchLineEdit
-)
-from qfluentwidgets import FluentIcon as FIF
+
+class MainWidget(QWidget):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self._border_visible = False
+        self._border_radius = 0
+        self._update_style()
+
+    def setBorderVisible(self, visible: bool):
+        self._border_visible = visible
+        self._update_style()
+
+    def setBorderRadius(self, radius: int):
+        self._border_radius = radius
+        self._update_style()
+
+    def _update_style(self):
+        style = ""
+        if self._border_visible:
+            style += f"border: 1px solid #333;"
+        if self._border_radius > 0:
+            style += f"border-radius: {self._border_radius}px;"
+        self.setStyleSheet(style)
 
 
 class MainWidget(QWidget):
-    """主页面"""
-
-    logoutRequested = Signal()  # 退出登录信号
+    """最简版表格测试"""
+    logoutRequested = Signal()
 
     def __init__(self, username=None):
         super().__init__()
-
-        self.username = username
-        self.walletWindow = None
-        self.rows = []
-
-        self.setWindowTitle("Tron 钱包")
+        self.setWindowTitle("表格展开测试")
+        self.resize(600, 300)
+        self.data = [
+            {"address": "Txxxxxxx1", "created_at": "2026-02-08 10:00"},
+            {"address": "Txxxxxxx2", "created_at": "2026-02-08 11:00"},
+            {"address": "Txxxxxxx3", "created_at": "2026-02-08 12:00"},
+            {"address": "Txxxxxxx3", "created_at": "2026-02-08 12:00"},
+            {"address": "Txxxxxxx3", "created_at": "2026-02-08 12:00"},
+            {"address": "Txxxxxxx3", "created_at": "2026-02-08 12:00"},
+            {"address": "Txxxxxxx3", "created_at": "2026-02-08 12:00"},
+            {"address": "Txxxxxxx3", "created_at": "2026-02-08 12:00"},
+            {"address": "Txxxxxxx3", "created_at": "2026-02-08 12:00"},
+            {"address": "Txxxxxxx3", "created_at": "2026-02-08 12:00"},
+            {"address": "Txxxxxxx3", "created_at": "2026-02-08 12:00"},
+            {"address": "Txxxxxxx3", "created_at": "2026-02-08 12:00"},
+            {"address": "Txxxxxxx3", "created_at": "2026-02-08 12:00"},
+            {"address": "Txxxxxxx3", "created_at": "2026-02-08 12:00"},
+            {"address": "Txxxxxxx3", "created_at": "2026-02-08 12:00"},
+            {"address": "Txxxxxxx3", "created_at": "2026-02-08 12:00"},
+            {"address": "Txxxxxxx3", "created_at": "2026-02-08 12:00"},
+            {"address": "Txxxxxxx3", "created_at": "2026-02-08 12:00"},
+            {"address": "Txxxxxxx3", "created_at": "2026-02-08 12:00"},
+            {"address": "Txxxxxxx3", "created_at": "2026-02-08 12:00"},
+            {"address": "Txxxxxxx3", "created_at": "2026-02-08 12:00"},
+            {"address": "Txxxxxxx3", "created_at": "2026-02-08 12:00"},
+            {"address": "Txxxxxxx3", "created_at": "2026-02-08 12:00"},
+            {"address": "Txxxxxxx3", "created_at": "2026-02-08 12:00"},
+            {"address": "Txxxxxxx3", "created_at": "2026-02-08 12:00"},
+            {"address": "Txxxxxxx3", "created_at": "2026-02-08 12:00"},
+            {"address": "Txxxxxxx3", "created_at": "2026-02-08 12:00"},
+        ]
+        self.layout = QVBoxLayout(self)
+        self.table = TableWidget()
+        self.layout.addWidget(self.table)
 
         # 创建钱包按钮
         self.addBtn = PrimaryPushButton()
@@ -52,58 +99,42 @@ class MainWidget(QWidget):
         self.logoutBtn = PrimaryToolButton()
         self.logoutBtn.setIcon(FIF.POWER_BUTTON)
         self.logoutBtn.clicked.connect(self.handleLogout)
-
-        # 表格
-        self.tableWidget = TableWidget()
-        self.tableWidget.setBorderVisible(True)
-        self.tableWidget.setBorderRadius(8)
-        self.tableWidget.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
-        self.tableWidget.setColumnCount(3)
-        self.tableWidget.setHorizontalHeaderLabels(["地址", "创建时间", "操作"])
-        self.tableWidget.setAlternatingRowColors(True)
-        self.tableWidget.setSelectionMode(QAbstractItemView.NoSelection)
-
-        # 设置每行高度
-        self.tableWidget.verticalHeader().setDefaultSectionSize(40)
-
-        # 设置列宽
-        header = self.tableWidget.horizontalHeader()
-        header.setSectionResizeMode(QHeaderView.Interactive)
-        header.setSectionResizeMode(0, QHeaderView.Stretch)
-        header.setSectionResizeMode(1, QHeaderView.Fixed)
-        self.tableWidget.setColumnWidth(1, 180)
-        header.setSectionResizeMode(2, QHeaderView.Fixed)
-        self.tableWidget.setColumnWidth(2, 60)
-
-        self.setupUi()
-
-    def getData(self, text: str):
-        print("搜索内容:", text)
-        # 可根据 text 过滤钱包列表
-        filtered = [
-            w for w in DBService.list_wallets() if text in w["address"]
-        ]
-        self.loadData(filtered)
-
-    def setupUi(self):
-        """创建界面"""
-        layout = VBoxLayout(self)
-        layout.setContentsMargins(20, 20, 20, 20)
-        layout.setSpacing(10)
-
-        # 顶部按钮和搜索
+        # 创建表格
+        self.table.setBorderVisible(True)
+        self.table.setBorderRadius(8)
+        self.table.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
+        # titleLine
         topLayout = QHBoxLayout()
         topLayout.addWidget(self.addBtn)
         topLayout.addWidget(self.searchEdit)
         topLayout.addStretch()
         topLayout.addWidget(self.logoutBtn)
-        layout.addLayout(topLayout)
+        self.layout.addLayout(topLayout)
 
-        # 表格拉伸占满剩余空间
-        layout.addWidget(self.tableWidget)
+        self.table.setColumnCount(3)
+        self.table.setHorizontalHeaderLabels(["地址", "创建时间", "操作"])
+        self.table.setAlternatingRowColors(True)
+        self.table.setSelectionMode(QAbstractItemView.NoSelection)
+        self.table.verticalHeader().setDefaultSectionSize(40)
 
-        # 加载初始数据
+        # 设置列宽
+        header = self.table.horizontalHeader()
+        header.setSectionResizeMode(QHeaderView.Interactive)
+        header.setSectionResizeMode(0, QHeaderView.Stretch)
+        header.setSectionResizeMode(1, QHeaderView.Fixed)
+        self.table.setColumnWidth(1, 180)
+        header.setSectionResizeMode(2, QHeaderView.Fixed)
+        self.table.setColumnWidth(2, 60)
+
+        self.layout.addWidget(self.table)
         self.loadData()
+        # for i, w in enumerate(wallets):
+        #     self.table.setItem(i, 0, QTableWidgetItem(w["address"]))
+        #     time_item = QTableWidgetItem(w["created_at"])
+        #     time_item.setTextAlignment(Qt.AlignCenter)
+        #     self.table.setItem(i, 1, time_item)
+        #     # 操作列用空白占位
+        #     self.table.setItem(i, 2, QTableWidgetItem(""))
 
     def createWallet(self):
         data = TronService.generate_wallet()
@@ -113,29 +144,19 @@ class MainWidget(QWidget):
         DBService.create_wallet(address, privateKey)
         self.loadData()
 
-        MessageBox.information(self, "创建成功", f"钱包地址：\n{address}")
-
-    def loadData(self, wallets=None):
-        """加载数据"""
-        if wallets is None:
-            wallets = DBService.list_wallets()
-        self.rows = wallets
-
-        self.tableWidget.setRowCount(len(wallets))
-
+    def loadData(self):
+        self.data = DBService.list_wallets()
+        self.table.setRowCount(len(self.data))
         # 更新搜索补全
-        addresses = [w["address"] for w in wallets]
-        self.completer.model().setStringList(addresses)
-
-        for index, data in enumerate(wallets):
+        for index, data in enumerate(self.data):
             # 地址列
             itemAddr = QTableWidgetItem(data["address"])
-            self.tableWidget.setItem(index, 0, itemAddr)
+            self.table.setItem(index, 0, itemAddr)
 
             # 时间列
             itemTime = QTableWidgetItem(data["created_at"])
             itemTime.setTextAlignment(Qt.AlignCenter)
-            self.tableWidget.setItem(index, 1, itemTime)
+            self.table.setItem(index, 1, itemTime)
 
             # 操作列按钮
             buttonWidget = QWidget()
@@ -149,13 +170,21 @@ class MainWidget(QWidget):
             enterWalletBtn.clicked.connect(partial(self.handleRowAction, index))
             buttonLayout.addWidget(enterWalletBtn, alignment=Qt.AlignCenter)
 
-            self.tableWidget.setCellWidget(index, 2, buttonWidget)
+            self.table.setCellWidget(index, 2, buttonWidget)
 
-    def handleRowAction(self, index: int):
-        row = self.rows[index]
-        self.walletWindow = WalletWidget(row)
-        self.walletWindow.show()
+    def getData(self):
+        pass
 
     def handleLogout(self):
-        self.logoutRequested.emit()
-        self.close()
+        pass
+
+    def handleRowAction(self, index):
+        print(index)
+        pass
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    demo = MainWidget()
+    demo.show()
+    sys.exit(app.exec())
